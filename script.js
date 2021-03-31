e8106e4490150e698961c350e829833bdabc4b72
Shubham Patil <shubham.patil9964123@gmail.com>
2021-03-31 16:25:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function first(){
+	document.getElementById('main').url="Image1.jpg";
+}
+function second(){
+	document.getElementById('main').url="Image2.webp";
+}
+setIntervel(first,2000);
+setIntervel(first,4000);
+
